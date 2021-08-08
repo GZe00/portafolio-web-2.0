8364a470cc871f18d02ca8536fe0d04812bbca61
Moises Fuentes <moyxess@gmail.com>
2021-08-08 08:09:28 -0500

    Js animation added

--- app.js
+++ app.js
@@ -9,3 +9,21 @@ iconMenu2.addEventListener('click', () => {
 iconMenu1.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+
+ScrollReveal().reveal('.about-me', {delay: 200});
+
+ScrollReveal().reveal('.so-1', {delay: 100});
+ScrollReveal().reveal('.so-2', {delay: 400});
+ScrollReveal().reveal('.so-3', {delay: 700});
+ScrollReveal().reveal('.so-4', {delay: 1000});
+ScrollReveal().reveal('.so-5', {delay: 1300});
+ScrollReveal().reveal('.so-6', {delay: 1600});
+ScrollReveal().reveal('.so-7', {delay: 1900});
+ScrollReveal().reveal('.so-8', {delay: 2100});
+ScrollReveal().reveal('.so-9', {delay: 100});
+ScrollReveal().reveal('.so-10', {delay: 400});
+ScrollReveal().reveal('.so-11', {delay: 700});
+ScrollReveal().reveal('.so-12', {delay: 1000});
+ScrollReveal().reveal('.so-13', {delay: 1300});
+ScrollReveal().reveal('.so-14', {delay: 1600});
