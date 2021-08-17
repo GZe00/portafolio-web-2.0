c98168299701f74de1a3d93c0313fb89d5c00059
Moises Fuentes <moyxess@gmail.com>
2021-08-17 14:34:27 -0500

    little animation added

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const iconMenu1 = document.querySelector('#ico-menu-1'), 
+const iconMenu1 = document.querySelector('#ico-menu-1'),
     iconMenu2 = document.querySelector('#ico-menu-2'),
     menu = document.querySelector('#nav');
 
@@ -10,8 +10,31 @@ iconMenu1.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
+gsap.from(".me", {duration: 3, x: 150, opacity: 0, scale: 0.5});
 
-ScrollReveal().reveal('.about-me', {delay: 200});
+gsap.to(".skill-box", {
+    y:50,
+    duration: 1.3,
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+        each: 0.1,
+        from: "random"
+    }
+});
+
+gsap.to(".social", {
+    y:30,
+    duration: 1.3,
+    repeat: -1,
+    yoyo: true,
+    stagger: {
+        each: 0.1,
+        from: "random"
+    }
+});
+
+// ScrollReveal().reveal('.about-me', {delay: 200});
 
 ScrollReveal().reveal('.so-1', {delay: 100});
 ScrollReveal().reveal('.so-2', {delay: 400});
@@ -21,9 +44,3 @@ ScrollReveal().reveal('.so-5', {delay: 1300});
 ScrollReveal().reveal('.so-6', {delay: 1600});
 ScrollReveal().reveal('.so-7', {delay: 1900});
 ScrollReveal().reveal('.so-8', {delay: 2100});
-ScrollReveal().reveal('.so-9', {delay: 100});
-ScrollReveal().reveal('.so-10', {delay: 400});
-ScrollReveal().reveal('.so-11', {delay: 700});
-ScrollReveal().reveal('.so-12', {delay: 1000});
-ScrollReveal().reveal('.so-13', {delay: 1300});
-ScrollReveal().reveal('.so-14', {delay: 1600});
